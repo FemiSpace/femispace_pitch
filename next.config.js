e9d7114623cc1058099375050be13446a18e8a32
FemiSpace <108429641+FemiSpace@users.noreply.github.com>
2025-02-13 13:34:52 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  env: {
-    PORT: 3001,
-  },
+  output: 'standalone'
 }
 
 module.exports = nextConfig
